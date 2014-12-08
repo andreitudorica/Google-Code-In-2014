42d94b6494c8b74f32eb8dd146df1be185675292
andreitudorica <andreitudoricacj@gmail.com>
2014-12-09 01:26:45 +0200

    Create JBUtils.java

---
+++ JBUtils.java
@@ -0,0 +1,137 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.*;
+import org.json.simple.parser.JSONParser;
+import org.bson.BSON;
+import org.bson.BSONObject;
+import org.bson.BasicBSONEncoder;
+import org.bson.BasicBSONDecoder;
+
+import com.mongodb.DBObject;
+import com.mongodb.util.*;
+
+public class JBUtils {
+	protected static BSONObject JsonToBson(JSONObject json)
+	{
+		BSONObject bson = (BSONObject) JSON.parse(JSON.serialize(json));
+		
+		return bson;
+	}
+	
+	protected static JSONObject BsonToJson(BSONObject bson)
+	{
+		String json_string = JSON.serialize(bson);
+		
+		JSONParser parser = new JSONParser();
+		
+		Object obj = null;
+		
+		try 
+		{
+			obj = parser.parse(json_string);
+		}
+		catch (ParseException e) 
+		{
+			e.printStackTrace();
+		}
+		JSONObject json = (JSONObject) obj;
+		
+		return json;
+	}
+	
+	protected static JSONObject ReadJSONfromFile(String filepath)
+   {
+		JSONParser parser = new JSONParser();
+		Object obj;
+		File file = new File(filepath);
+		JSONObject json = new JSONObject();
+		try {
+			obj = parser.parse(new FileReader(file));
+			json = (JSONObject) obj;
+		} 
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		return json;
+	}
+	
+	protected static BSONObject ReadBSONfromFile(String filepath) 
+	{
+		File file = new File(filepath);
+		FileInputStream fileInputStream=null;
+        byte[] bson_byte = new byte[(int) file.length()];
+ 
+        try {
+	    fileInputStream = new FileInputStream(file);
+	    fileInputStream.read(bson_byte);
+	    fileInputStream.close();
+
+        }catch(Exception e){
+        	e.printStackTrace();
+        }
+        BasicBSONDecoder decoder = new BasicBSONDecoder();
+		BSONObject bsonObject = decoder.readObject(bson_byte);
+		return bsonObject;
+	}
+	protected static void PrintJSONtoFile(JSONObject json,String filepath)
+	{
+		try {
+	          File file = new File(filepath);
+	          BufferedWriter output = new BufferedWriter(new FileWriter(file));
+	          String text= (String) json.toString();
+	          output.write(text);
+	          output.close();
+	        } catch ( IOException e ) {
+	           e.printStackTrace();
+	        }
+	}
+	protected static void PrintBSONtoFileDecoded(BSONObject bson,String filepath)
+
+	{
+		try {
+	          File file = new File(filepath);
+	          BufferedWriter output = new BufferedWriter(new FileWriter(file));
+	          String text= (String) bson.toString();
+	          output.write(text);
+	          output.close();
+	        } catch ( IOException e ) {
+	           e.printStackTrace();
+	        }
+	}
+	
+	protected static void PrintBSONtoFileEncoded(BSONObject bson,String filepath)
+	{
+		BasicBSONEncoder encoder = new BasicBSONEncoder();
+		byte[] bson_bytes = encoder.encode(bson);
+		
+		
+		try {
+	          File file = new File(filepath);
+	          BufferedWriter output = new BufferedWriter(new FileWriter(file));
+	      String text = "";
+
+	    for(int i = 0; i < bson_bytes.length; i++)
+	    {
+	        text += (char)bson_bytes[i];
+	    }
+	    output.write(text);
+        output.close();
+      } catch ( IOException e ) {
+         e.printStackTrace();
+      }
+	}
+}
