f079458a00a3bc7eeffc2a8abcea9cd3fab55155
andreitudorica <andreitudoricacj@gmail.com>
2014-12-09 06:34:08 +0200

    Update JBUtils.java

--- JBUtils.java
+++ JBUtils.java
@@ -53,6 +53,7 @@ public class JBUtils {
    {
 		JSONParser parser = new JSONParser();
 		Object obj;
+		filepath=filepath+".json";
 		File file = new File(filepath);
 		JSONObject json = new JSONObject();
 		try {
@@ -71,30 +72,43 @@ public class JBUtils {
 	
 	protected static BSONObject ReadBSONfromFile(String filepath) 
 	{
-		File file = new File(filepath);
+		filepath=filepath+".bson";
+    	File file = new File(filepath);
 		FileInputStream fileInputStream=null;
         byte[] bson_byte = new byte[(int) file.length()];
- 
-        try {
-	    fileInputStream = new FileInputStream(file);
-	    fileInputStream.read(bson_byte);
-	    fileInputStream.close();
-
+        try 
+        {
+        	fileInputStream = new FileInputStream(file);
+        	fileInputStream.read(bson_byte);
+        	fileInputStream.close();
         }catch(Exception e){
         	e.printStackTrace();
         }
         BasicBSONDecoder decoder = new BasicBSONDecoder();
-		BSONObject bsonObject = decoder.readObject(bson_byte);
+        BSONObject bsonObject = decoder.readObject(bson_byte);
 		return bsonObject;
 	}
 	protected static void PrintJSONtoFile(JSONObject json,String filepath)
 	{
+		filepath=filepath+".json";
 		try {
 	          File file = new File(filepath);
-	          BufferedWriter output = new BufferedWriter(new FileWriter(file));
-	          String text= (String) json.toString();
-	          output.write(text);
-	          output.close();
+	          if(file.createNewFile())
+	          {
+	        	  BufferedWriter output = new BufferedWriter(new FileWriter(file));
+	        	  String text= (String) json.toString();
+	        	  output.write(text);
+	        	  output.close();
+	          }
+	          else
+	          {
+	        	  file.delete();
+	        	  file.createNewFile();
+	        	  BufferedWriter output = new BufferedWriter(new FileWriter(file));
+	        	  String text= (String) json.toString();
+	        	  output.write(text);
+	        	  output.close();
+	          }
 	        } catch ( IOException e ) {
 	           e.printStackTrace();
 	        }
@@ -102,12 +116,25 @@ public class JBUtils {
 	protected static void PrintBSONtoFileDecoded(BSONObject bson,String filepath)
 
 	{
+		filepath=filepath+"_decoded.bson";
 		try {
 	          File file = new File(filepath);
-	          BufferedWriter output = new BufferedWriter(new FileWriter(file));
-	          String text= (String) bson.toString();
-	          output.write(text);
-	          output.close();
+	          if(file.createNewFile())
+	          {
+	        	  BufferedWriter output = new BufferedWriter(new FileWriter(file));
+	        	  String text= (String) bson.toString();
+	        	  output.write(text);
+	        	  output.close();
+	          }
+	          else
+	          {
+	        	  file.delete();
+	        	  file.createNewFile();
+	        	  BufferedWriter output = new BufferedWriter(new FileWriter(file));
+		          String text= (String) bson.toString();
+		          output.write(text);
+		          output.close();
+	          }
 	        } catch ( IOException e ) {
 	           e.printStackTrace();
 	        }
@@ -115,21 +142,39 @@ public class JBUtils {
 	
 	protected static void PrintBSONtoFileEncoded(BSONObject bson,String filepath)
 	{
+		filepath=filepath+"_encoded.bson";
 		BasicBSONEncoder encoder = new BasicBSONEncoder();
 		byte[] bson_bytes = encoder.encode(bson);
 		
 		
-		try {
-	          File file = new File(filepath);
-	          BufferedWriter output = new BufferedWriter(new FileWriter(file));
-	      String text = "";
-
-	    for(int i = 0; i < bson_bytes.length; i++)
-	    {
-	        text += (char)bson_bytes[i];
-	    }
-	    output.write(text);
-        output.close();
+		try 
+		{
+	        File file = new File(filepath);
+	        if(file.createNewFile())
+	        {
+	        	BufferedWriter output = new BufferedWriter(new FileWriter(file));
+	        	String text = "";
+	        	for(int i = 0; i < bson_bytes.length; i++)
+	        	{
+	        		text += (char)bson_bytes[i];
+	        	}
+	        	output.write(text);
+	        	output.close();
+			}
+	        else 
+	        {
+	        	file.delete();
+	        	file.createNewFile();
+	        	BufferedWriter output = new BufferedWriter(new FileWriter(file));
+	        	String text = "";
+	        	for(int i = 0; i < bson_bytes.length; i++)
+	        	{
+	        		text += (char)bson_bytes[i];
+	        	}
+	        	output.write(text);
+	        	output.close();
+	        }
+	        
       } catch ( IOException e ) {
          e.printStackTrace();
       }
